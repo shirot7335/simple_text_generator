d357a73955d514728be1706d145f174702ebb2f1
Ryoichi Suganuma <ryoichi.suganuma@satori.team>
2022-01-17 13:49:05 +0900

    add template file for example

---
+++ example/example.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+curdir="$(cd $(dirname $0) && pwd)"
+
+template_file_dir="${curdir}/template"
+dist_file_dir="${curdir}/dist"
+
+cd "${curdir}/.."
+
+./simple_text_generator \
+    --template="${template_file_dir}/template.txt" \
+    --dist="${dist_file_dir}/result.txt" \
+    --greeting="Hello" \
+    --target="world"
+
+echo '---template---'
+cat "${template_file_dir}/template.txt"
+echo '--------------'
+
+echo
+
+echo '-----dist-----'
+cat "${dist_file_dir}/result.txt"
+echo '--------------'
+
+exit 0
